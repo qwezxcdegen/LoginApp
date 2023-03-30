efc557d18c1eb13dabefddb18fbe81589adddf4a
qwezxcdegen <ifomstep@icloud.com>
2023-03-31 01:05:58 +0500

    Setup loginVC

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak private var userNameTF: UITextField!
+    
+    @IBOutlet weak private var passwordTF: UITextField!
+    
+    private let adminLogin = "Admin"
+    private let adminPassword = "1234"
 
+    
+    @IBAction private func loginButtonPressed() {
+        guard let login = userNameTF.text, let password = passwordTF.text
+        else {
+            return
+        }
+        guard !login.isEmpty, !password.isEmpty else {
+            showAlert(title: "Error", message: "Login or password is empty")
+            return
+        }
+        guard login == adminLogin, password == adminPassword else {
+            showAlert(title: "Error", message: "Login or password is wrong")
+            return
+        }
+        
+    }
+    @IBAction private func forgotUserNamePressed() {
+        showAlert(title: "Oops!", message: "Your name is \(adminLogin)")
+    }
+    
+    @IBAction private func forgotPasswordPressed() {
+        showAlert(title: "Oops!", message: "Your password is \(adminPassword)")
+    }
+    
+    private func showAlert(title: String?, message: String?) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTF.text = ""
+        }
+        
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true)
+    }
 
 }
 
